adc40e198ff465ada9d6368ff868624d7da4e8db
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2026-03-06 16:39:15 +0300

    add random weight func

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const MAX_ANGLE = 30;
 
 const MIN_WEIGHT = 1;
 const MAX_WEIGHT = 10;
+
+function getRandomWeight() {
+  return Math.floor(Math.random() * (MAX_WEIGHT - MIN_WEIGHT + 1)) + MIN_WEIGHT;
+}
