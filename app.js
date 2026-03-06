dd29bca893a8758d128522fcefe43ae004607975
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2026-03-06 16:41:04 +0300

    add state obj

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const MAX_ANGLE = 30;
 const MIN_WEIGHT = 1;
 const MAX_WEIGHT = 10;
 
+let state = {
+  objects: [],
+  angle: 0,
+  nextWeight: getRandomWeight(),
+}
+
 function getRandomWeight() {
   return Math.floor(Math.random() * (MAX_WEIGHT - MIN_WEIGHT + 1)) + MIN_WEIGHT;
 }
